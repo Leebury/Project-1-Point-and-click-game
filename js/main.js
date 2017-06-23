bd1871c1f8ebc9766b284b7ea04ba0de505ca70c
Leebury <lbury@spartaglobal.com>
2017-06-23 16:17:09 +0100

    Added a score tally, Next task is to add the image

--- js/main.js
+++ js/main.js
@@ -1,1 +1,31 @@
 console.log('Linked up');
+
+$ (function(){
+
+
+var $scoreDisplay = $('#score');
+var $main = $('#main');
+var $target = $('.target');
+var score = 0;
+
+//Change to target when needed
+	$main.on('click', function(event){
+		console.log('Body click');
+		incrementScore();
+	});
+
+	function incrementScore (){
+
+		score += 10;
+		$scoreDisplay.html(score);
+	}
+
+
+//For now use div to add click function
+//On click adds 10 to the score
+
+
+
+
+
+});
