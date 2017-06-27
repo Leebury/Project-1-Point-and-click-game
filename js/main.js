5bb3303a4614e539f1ebef26d40bddcf5f4741d1
Leebury <lbury@spartaglobal.com>
2017-06-27 14:05:28 +0100

    Bugfixes, ending screen working

--- js/main.js
+++ js/main.js
@@ -1,44 +1,45 @@
 console.log('Linked up');
 
-$ (function(){
+$(function(){
 
 
-var $scoreDisplay = $('#score');
-var $main = $('#main');
-var $timeDisplay = $('#time')
-// var $etarget = $('.easyTarget');
-// var $mtarget
-// var $htarget = $('.hardTarget');
+	var $scoreDisplay = $('#score');
+	var $main = $('#main');
+	var $timeDisplay = $('#time')
+	// var $etarget = $('.easyTarget');
+	// var $mtarget
+	// var $htarget = $('.hardTarget');
 
-var $targets = $('.hardTarget, .easyTarget');
-var score = 0;
-var time = 60;
-var countdown= setInterval(timesUp, 1000);
-var easyTargetArray = ['#target0', '#target1'];
-var hardTargetArray = ['#target2'];
-var $target0 = $('#target0')
-var $target1 = $('#target1')
-var interval = setInterval(function(){
-	animation();
-}, 3000);
+	var $targets = $('.hardTarget, .easyTarget');
+	var score = 0;
+	var time = 10;
+	var countdown= setInterval(timesUp, 1000);
+	var easyTargetArray = ['#target0', '#target1'];
+	var hardTargetArray = ['#target2'];
+	var $target0 = $('#target0')
+	var $target1 = $('#target1')
+	var interval = setInterval(function(){
+		animation();
+	}, 3000);
 
-
+	$('#finish').hide();
 	//this only works for hitting easy targets
 	//need to find a way to have the click respond based on what's clicked
 	//Pref without making an entire new function for each target
 
 	$targets.on('click', function(event){
-		var $this = $(this);
-		
+		var $this = $(this);		
 		var pointsToIncrease = $this.data('points');
+		// debugger
 		incrementScore(pointsToIncrease);
-
 		$(this).fadeOut();
 		$(this).hide();
-		// $(this).stop();
+		$(this).stop();
+
 		setTimeout(function(){
-			$this.css('bottom', '-100px');
-			// $this.fadeIn(); 
+			//if this.data = left/right/bottom
+			$this.css('bottom', '-100px');	
+			$this.show();
 		}, 2000);
 
 
@@ -51,13 +52,25 @@ var interval = setInterval(function(){
 	};
 
 	function timesUp(){
- 	 time=time-1
- 	 if (time <= -1){
- 	 	clearInterval(countdown);
+	 	time=time-1
+	 	if (time <= -1){
+	 	 	clearInterval(countdown);
+	 	 	return;
+	 	}
+	 	$("#timer").html(time + " seconds");
+	 	if (time == 0){
+			$('#main').hide();
+			$('#finish').show();
+			$targets.stop();
+			clearInterval(interval);
+	 	};
+	};
 
- 	 	return;
- 	 }
- 	 document.getElementById("timer").innerHTML=time + " seconds";
+	function endScore(){
+
+		var finalScore=$scoreDisplay.html();
+
+		finalScore.append('#finalScore');
 	}
 	
 
@@ -65,7 +78,6 @@ var interval = setInterval(function(){
 	function getRandomTarget () {
 
 		var randomNumber = Math.floor(Math.random()*( $targets.length - 0))
-
 		return $('.easyTarget:not(:animated), .hardTarget:not(:animated)').eq(randomNumber);
 	}
 
@@ -74,16 +86,14 @@ var interval = setInterval(function(){
 		var selection = getRandomTarget()
 		console.log(selection);
 
-
-
 		if (selection.hasClass('easyTarget')) {
 			// var string = $(selection);
 			selection.fadeIn();
 			selection.show();
 			selection.animate({bottom:'100px'},7000,function(){
-				setTimeout(function(){
-					selection.animate({bottom:'-100px'}, 5000);
-				},7000);
+				setTimeout(function () {
+					selection.animate({ bottom: '-100px' }, 5000);
+				}, 7000);
 			});
 
 		} else if (selection.hasClass('hardTarget')) {
